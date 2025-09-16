dec394ee04aa4bf7d55535401dff7aeeba22a1b7
Jan Klacan <156678842+jan-klacan@users.noreply.github.com>
2025-09-16 14:17:42 +0200

    String evolver create + fitness

---
+++ string-evolver.py
@@ -0,0 +1,35 @@
+import sys
+import string
+import math
+import random
+import argparse
+import matplotlib.pyplot as plt
+
+# --- FITNESS ---
+
+def fitness_Matches_Exact(candidate, target):
+    """
+    Purpose: count exact character matches between a candidate and target
+                - if lengths of the strings differ, compare up to the shorter length
+    Returns: integer
+    """
+    exact_matches = 0
+    length = min(len(candidate), len(target))
+
+    for i in range(length):
+        if candidate[i] == target[i]:
+            exact_matches += 1
+
+    return exact_matches
+
+
+def fitness_Matches_Normalized(candidate, target):
+    """
+    Purpose: normalize exact matches for a string to an interval <0.0, 1.0>
+                - 1.0 means 100% match on all target characters
+    Returns: float between 0.0 and 1.0
+    """
+    if len(target) == 0:
+        return 1.0 # this is an edge case (empty target as already matched)
+    matches = fitness_Matches_Exact(candidate, target)
+    return matches / len(target)
